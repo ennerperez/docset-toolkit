7b3768f72e9629317a8f939b6e57f4385f13f2d8
Enner Pérez <ennerperez@gmail.com>
2018-04-03 10:51:19 -0500

    Initial UI

---
+++ src/ZDocs.Core/Models/Docset.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using PListNet;
+using System.Collections.Generic;
+
+namespace ZDocs.Core.Models
+{
+    public class Docset
+    {
+        public Docset()
+        {
+            Index = new HashSet<Index>();
+        }
+
+        [JsonIgnore]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Revision { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+
+        public override string ToString()
+        {
+            return Title ?? base.ToString();
+        }
+
+        [JsonIgnore]
+        public string Path { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Index> Index { get; set; }
+
+        [JsonIgnore]
+        public ICollection<string> Icons { get; set; }
+
+        [JsonIgnore]
+        public string License { get; set; }
+
+        [JsonIgnore]
+        public PNode Plist { get; set; }
+    }
+}
--- src/ZDocs.Shell/FormMain.Designer.cs
+++ src/ZDocs.Shell/FormMain.Designer.cs
@@ -29,23 +29,59 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.treeViewContent = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // treeViewContent
+            // 
+            this.treeViewContent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeViewContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewContent.Location = new System.Drawing.Point(0, 0);
+            this.treeViewContent.Name = "treeViewContent";
+            this.treeViewContent.Size = new System.Drawing.Size(208, 441);
+            this.treeViewContent.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeViewContent);
+            this.splitContainer1.Size = new System.Drawing.Size(624, 441);
+            this.splitContainer1.SplitterDistance = 208;
+            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.TabIndex = 1;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(640, 480);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ZDocs";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeViewContent;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
