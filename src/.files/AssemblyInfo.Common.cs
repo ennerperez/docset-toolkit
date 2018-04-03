679f47e3c70241d4d351ab957087eca52bf8a3ad
Enner Pérez <ennerperez@gmail.com>
2018-04-03 10:42:40 -0500

    Initial solution

--- src/.files/AssemblyInfo.Common.cs
+++ src/.files/AssemblyInfo.Common.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("Offline documentation browser")]
+[assembly: AssemblyCompany("Enner Pérez")]
+[assembly: AssemblyProduct("ZDocs")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
